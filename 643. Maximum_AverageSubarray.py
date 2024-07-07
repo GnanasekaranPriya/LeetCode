d84995275f15a0428ea1168990856ef73a473b7b
Priya Gnanasekaran <gnanapriya40@gmail.com>
2024-07-06 23:24:54 -0500

    Update 643. Maximum_AverageSubarray.py

--- 643. Maximum_AverageSubarray.py
+++ 643. Maximum_AverageSubarray.py
@@ -6,13 +6,13 @@ class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         l = 0 
         r = k 
-        final = sum(nums[l:r])
-        current_val = final 
+        avg_sum = sum(nums[l:r])
+        current_val = avg_sum 
         while r < len(nums):
             current_val = current_val - nums[l] + nums[r]
-            if current_val > final: 
-                final = current_val 
+            if current_val > avg_sum: 
+                avg_sum = current_val 
             l += 1
             r += 1
-        return final/k 
+        return avg_sum/k 
 
